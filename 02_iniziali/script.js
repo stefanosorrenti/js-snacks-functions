bbf50353f4d10f964cf6b8963b9f0dfacde6d1b5
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-10 16:17:31 +0100

    Add 02 - iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,12 +1,40 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
+//Lista di nomi da cui ricavarci il primo carattare per ogni nome.
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
-// Dichiara la funzione qui.
+
+/**
+ * ##First character
+ * This function allows you to retrieve the first character of each name
+ * @param {array} array - Enter the array you want to analyze
+ * @returns {array} - returns an array with the first characters of each name
+*/
+
+
+// Dichiara la funzione qui. 
+function getFirstChar(array) { 
+
+    const firstChar = [] //creo una varaibile d'appoggio per inserire il mio nuovo array
+
+    for (i = 0; i < array.length; i++) { //ciclo ogni elemento per tutta la lunghezza dell'array inserito
+
+        const char = array[i] //salvo in una variabile ogni elemento ciclato dell'aaray isnerito.
+        firstChar.push(char[0]) //pusho nella variabile d'appoggio ogni primo carattere di ogni primo elemento ciclato dell'array inserito
+
+    }
+
+    return firstChar //la funzione restituisce l'array pushato
+
+}
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
+const firstCharNames = getFirstChar(names) //salvo la funzione invocato con l'argomento in nuova variabile per differenziarmi dall'esercizio prima.
+console.log(firstCharNames); //stampo la mia variabile che conterrà la funzione a cui è stata associato return fistChar
+
 
 
 
