69d076e6430046e3e2e6365a19e5aeced08ce4cc
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-10 15:24:34 +0100

    Add 01 - saluti

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,12 +1,34 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const userName = 'Mario';
+
+//Inserisci il nome
+const userName = prompt('Inserisci il tuo nome per essere salutato!');
+
+
 
 
 // Dichiara la funzione qui.
+/**
+ * ##Say hello!
+ * This feature greets the user using his name
+ * @param {string} promptName - the prompt that allows you to write the name must be inserted 
+ * @returns {string} - returns the user's name + 'Hello'
+ */
+
+//La funzione deve salutare l'utente
+function getHello(promptName) { // si inserisce il nome dell'utente
+
+    const hello = `Ciao ${promptName}!` //la costante unisce 'ciao' è il nome dell'utente
+    
+    return hello //la funzione resitutisce la costante
+
+}
+
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
+console.log(getHello(userName));
 
 
 
