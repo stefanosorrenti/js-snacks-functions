f1cbf64e207673ada9a3944eb5f019ba4e6afce8
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-10 20:01:18 +0100

    Add arro function for 04 - conta vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -12,25 +12,40 @@ const word = 'javascript';
  * @param {string} string - Enter the word to test.
  * @returns {number} - Returns the number of vowels found.
  */
-function getNumbOfVowels (string) {
-   
-    const numbVowels =[] //creo una variabile d'appoggio
-    
-    for(i = 0; i < string.length; i++) { //ciclo ogni elemento per tutta la lunghezza della stringa inserito
+function getNumbOfVowels(string) {
+
+    const numbVowels = [] //creo una variabile d'appoggio
+
+    for (i = 0; i < string.length; i++) { //ciclo ogni elemento per tutta la lunghezza della stringa inserito
         const char = string[i] //salvo in una variabile ogni elemento ciclato dell'array inserito. (in questo caso saranno direttamente i caratteri)
-        
-        if(char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u') { //SE char è ugale ad a O e O i ecc ecc
+
+        if (char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u') { //SE char è ugale ad a O e O i ecc ecc
             numbVowels.push(char) //pusho l'elemento in iterazione all'array d'appoggio
-        }  
-        
+        }
+
     }
 
     return numbVowels.length //associo alla funzione il valore della lunghezza della nostra variabile d'appoggio in modo tale da avere un numero.
 }
 
+//ARROW FUNCTION
+/* const getNumbOfVowels = (string) => {
 
+    const numbVowels = [] //creo una variabile d'appoggio
 
+    for (i = 0; i < string.length; i++) { //ciclo ogni elemento per tutta la lunghezza della stringa inserito
+        const char = string[i] //salvo in una variabile ogni elemento ciclato dell'array inserito. (in questo caso saranno direttamente i caratteri)
 
+        if (char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u') { //SE char è ugale ad a O e O i ecc ecc
+            numbVowels.push(char) //pusho l'elemento in iterazione all'array d'appoggio
+        }
+
+    }
+
+    return numbVowels.length //associo alla funzione il valore della lunghezza della nostra variabile d'appoggio in modo tale da avere un numero.
+}
+
+ */
 // Invoca la funzione qui e stampa il risultato in console
 console.log(getNumbOfVowels(word));
 
